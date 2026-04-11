918bedd7eb9b2cd575894bcf1ad2d6b1e11e9688
WildhanValerian25 <wildanvalerian89@gmail.com>
2026-04-11 17:30:27 +0700

    mengisi ComboBox jenis kelamin dan mengatur konfigurasi DataGridView"

--- PisangGoreng_DRCRUD/Form1.cs
+++ PisangGoreng_DRCRUD/Form1.cs
@@ -11,6 +11,27 @@ namespace PisangGoreng_DRCRUD
         private readonly string connectionString =
             "Data Source=LAPTOP-24A5CGHI\\WILDHANFIGHT.DBAkademikADO;Integrated Security=True";
 
-        
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new SqlConnection(connectionString);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbJK.Items.Clear();
+            cmbJK.Items.Add("L");
+            cmbJK.Items.Add("P");
+
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.MultiSelect = false;
+            dataGridView1.ReadOnly = true;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            dataGridView1.CellClick += dataGridView1_CellClick;
+        }
+
+       
     }
 }
