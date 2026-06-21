a5363781ba9a42027bdd08e61538b0fda811cde4
WildhanValerian25 <wildanvalerian89@gmail.com>
2026-06-21 14:21:41 +0700

    Add Data and Dataset helper class

---
+++ PisangGoreng_DRCRUD/Data.cs
@@ -0,0 +1,18 @@
+﻿using CrystalDecisions.ReportSource;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PisangGoreng_DRCRUD
+{
+    internal class Data
+    {
+        public String Nama { get; set; }
+        public String JenisKelamin { get; set;}
+        public String Alamat { get; set; }
+        public String NamaProdi { get; set; }
+        public DateTime TanggalDaftar { get; set; }
+    }
+}
