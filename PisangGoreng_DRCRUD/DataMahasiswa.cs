37ba29b84978e4b2899e7f7e709bef53258126b0
WildhanValerian25 <wildanvalerian89@gmail.com>
2026-06-21 14:21:06 +0700

    Add DataMahasiswa and ListMahasiswa model class

---
+++ PisangGoreng_DRCRUD/DataMahasiswa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PisangGoreng_DRCRUD
+{
+    public class DataMahasiswa
+    {
+        public string Nama { get; set; }
+        public string JenisKelamin { get; set; }
+        public string Alamat { get; set; }
+        public string NamaProdi { get; set; }
+        public DateTime TanggalDaftar { get; set; }
+    }
+
+    public class ListMahasiswa : List<DataMahasiswa>
+    {
+        // kosong, hanya wrapper
+    }
+}
