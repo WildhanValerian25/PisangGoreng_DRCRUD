9dcf1da7216e4251e71d2c8dbb5c929da16eb84b
WildhanValerian25 <wildanvalerian89@gmail.com>
2026-06-21 14:24:49 +0700

    Set FormDashboard as startup form

--- PisangGoreng_DRCRUD/Program.cs
+++ PisangGoreng_DRCRUD/Program.cs
@@ -16,7 +16,7 @@ namespace PisangGoreng_DRCRUD
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormDashboard()); // ← ubah ini
         }
     }
 }
